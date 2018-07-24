82b5e02780d1689a16f883421e86c7c5d9a2f3c8
Marc Williamson <mew488@PD326.PHYSICS.NYU.EDU>
2018-07-24 17:09:33 -0400

    added wavelength filtering, NaN replacement, remove phases from SNIDsn object.

--- SNIDsn.py
+++ SNIDsn.py
@@ -6,6 +6,9 @@ import pickle
 
 
 def getType(tp, subtp):
+    """
+Convert tuple type designation from SNID to string.
+    """
     if tp == 1:
         sntype = 'Ia'
         if subtp == 2: snsubtype = 'norm'
@@ -69,6 +72,11 @@ class SNIDsn:
         return
 
     def loadSNIDlnw(self, lnwfile):
+        """
+Loads a SNID .lnw file into a SNIDsn object. Header information from the .lnw file \
+is stored in a header dictionary. Other data is stored in the following fields: continuum, phases,\
+phaseType, wavelengths, data, type, subtype.
+        """
         with open(lnwfile) as lnw:
             lines = lnw.readlines()
             lnw.close()
@@ -113,7 +121,7 @@ class SNIDsn:
             dt = (colname, 'f4')
             lnwdtype.append(dt)
         #lnwdtype = [('Ph'+str(ph), 'f4') for ph in self.phases]
-        data = np.loadtxt(lnwfile, dtype=lnwdtype, skiprows=phase_line_ind + 1)
+        data = np.loadtxt(lnwfile, dtype=lnwdtype, skiprows=phase_line_ind + 1, usecols=range(1,len(self.phases) + 1))
         self.data = data
 
         continuumcols = len(lines[1].strip().split())
@@ -124,7 +132,58 @@ class SNIDsn:
         self.continuum = continuum
         return
 
+    def wavelengthFilter(self, wvlmin, wvlmax):
+        """
+Filters the wavelengths to a user specified range, and adjusts the spectra data\
+array to reflect that wavelength range.
+        """
+        wvlfilter = np.logical_and(self.wavelengths < wvlmax, self.wavelengths > wvlmin)
+        wvl = self.wavelengths[wvlfilter]
+        self.data = self.data[wvlfilter]
+        self.wavelengths = self.wavelengths[wvlfilter]
+        return
+       
+    def removeSpecCol(self, colname):
+        """
+Removes the column named colname from the structured spectra matrix. Removes the \
+phase from the list of phases.
+        """
+        newdtype = [(dt[0], dt[1]) for dt in self.data.dtype.descr if dt[0] != colname]
+        newshape = (len(self.data),len(newdtype))
+        ndarr = np.ndarray(newshape)
+        for i in range(len(newdtype)):
+            ndtype = newdtype[i]
+            nm = ndtype[0]
+            ndarr[:,i] = self.data[nm]
+        newstructarr = np.array([tuple(row.tolist()) for row in ndarr], dtype=newdtype)
+        newphases = self.phases.tolist()
+        newphases.remove(float(colname[2:]))
+        self.phases = np.array(newphases)
+        self.data = newstructarr
+        return
+
+
+    def snidNAN(self):
+        """
+SNID uses 0.0 as a placeholder value when observed data is not available for \
+a certain wavelength. This method replaces all 0.0 values with NaN.
+        """
+        colnames = self.getSNCols()
+        for col in colnames:
+            self.data[col][self.data[col] == 0] = np.nan
+        return 
+
+    def getSNCols(self):
+        """
+Returns spectra structured array column names for the user.
+        """
+        return self.data.dtype.names
+
     def save(self, path='./'):
+        """
+Saves the SNIDsn object using pickle. Filename is automatically generated using \
+the sn name stored in the SNIDsn.header['SN'] field.
+        """
         filename = self.header['SN']
         f = open(path+filename+'.pickle', 'w')
         pickle.dump(self, f)
