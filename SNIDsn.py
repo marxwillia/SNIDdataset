9561bef8285b54bde368c2abde86a7fdabfc2e32
Marc Williamson <mew488@PD326.PHYSICS.NYU.EDU>
2018-07-25 18:16:09 -0400

    implemented smoothing

--- SNIDsn.py
+++ SNIDsn.py
@@ -1,6 +1,9 @@
 from __future__ import division
 import numpy as np
 import pickle
+import scipy
+import scipy.stats as st
+import scipy.optimize as opt
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -79,6 +82,83 @@ intersects the specified wavelength range.
     return gapInRange
 
 
+# Binspec implemented in python.
+def binspec(wvl, flux, wstart, wend, wbin):
+    nlam = (wend - wstart) / wbin + 1
+    nlam = int(np.ceil(nlam))
+    outlam = np.arange(nlam) * wbin + wstart
+    answer = np.zeros(nlam)
+    interplam = np.unique(np.concatenate((wvl, outlam)))
+    interpflux = np.interp(interplam, wvl, flux)
+
+    for i in np.arange(0, nlam - 1):
+        cond = np.logical_and(interplam >= outlam[i], interplam <= outlam[i+1])
+        w = np.where(cond)
+        if len(w) == 2:
+            answer[i] = 0.5*(np.sum(interpflux[cond])*wbin)
+        else:
+            answer[i] = scipy.integrate.simps(interpflux[cond], interplam[cond])
+
+    answer[nlam - 1] = answer[nlam - 2]
+    cond = np.logical_or(outlam >= max(wvl), outlam < min(wvl))
+    answer[cond] = 0
+    return answer/wbin, outlam
+
+#smooth spectrum using SNspecFFTsmooth procedure
+def smooth(wvl, flux, cut_vel):
+    c_kms = 299792.47 # speed of light in km/s
+    vel_toolarge = 100000 # km/s
+
+    wvl_ln = np.log(wvl)
+    num = wvl_ln.shape[0]
+    binsize = wvl_ln[-1] - wvl_ln[-2]
+    f_bin, wln_bin = binspec(wvl_ln, flux, min(wvl_ln), max(wvl_ln), binsize)
+    fbin_ft = np.fft.fft(f_bin)#*len(f_bin)
+    freq = np.fft.fftfreq(num)
+    num_upper = np.max(np.where(1.0/freq[1:] * c_kms * binsize > cut_vel))
+    num_lower = np.max(np.where(1.0/freq[1:] * c_kms * binsize > vel_toolarge))
+    mag_avg = np.mean(np.abs(fbin_ft[num_lower:num_upper+1]))
+    powerlaw = lambda x, amp, exp: amp*x**exp
+#    print num_upper
+#    print nup
+    num_bin = len(f_bin)
+    xdat = freq[num_lower:num_upper]
+#    print xdat
+    ydat = np.abs(fbin_ft[num_lower:num_upper])
+#    print ydat
+    nonzero_mask = xdat!=0
+    slope, intercept, _,_,_ = st.linregress(np.log(xdat[nonzero_mask]), np.log(ydat[nonzero_mask]))
+    exp_guess = slope
+    amp_guess = np.exp(intercept)
+#    print slope, intercept
+
+    #do powerlaw fit
+    xdat = freq[num_lower:int(num_bin/2)]
+    ydat = np.abs(fbin_ft[num_lower:int(num_bin/2)])
+    #exclude data where x=0 because this can cause 1/0 errors if exp < 0
+    finite_mask = np.logical_not(xdat==0)
+    finite_mask = np.logical_and(finite_mask, np.isfinite(ydat))
+    ampfit, expfit = opt.curve_fit(powerlaw, xdat[finite_mask], ydat[finite_mask], p0=[amp_guess, exp_guess])[0]
+
+    #find intersection of average fbin_ft magnitude and powerlaw fit to calculate separation
+    #velocity between signal and noise.
+    intersect_x = np.power((mag_avg/ampfit), 1.0/expfit)
+    sep_vel = 1.0/intersect_x * c_kms * binsize
+
+    #filter out frequencies with velocities higher than sep_vel
+    smooth_fbin_ft = np.array([fbin_ft[ind] if np.abs(freq[ind])<np.abs(intersect_x) else 0 \
+                               for ind in range(len(freq))])#/len(f_bin)
+    #inverse fft on smoothed fluxes
+    smooth_fbin_ft_inv = np.real(np.fft.ifft(smooth_fbin_ft))
+
+    #interpolate smoothed fluxes back onto original wavelengths
+    w_smoothed = np.exp(wln_bin)
+    f_smoothed = np.interp(wvl, w_smoothed, smooth_fbin_ft_inv)
+
+    return w_smoothed, f_smoothed, sep_vel
+
+
+
 class SNIDsn:
     def __init__(self):
         self.header = None
@@ -89,6 +169,8 @@ class SNIDsn:
         self.data = None
         self.type = None
         self.subtype = None
+
+        self.smoothinfo = dict()
 
         return
 
@@ -210,6 +292,8 @@ Returns a list of all the gaps present in the spectrum for the given phase.
         nanind = np.argwhere(np.isnan(spec)).flatten()
         nanwvl = wvl[np.isnan(spec)]
         gaps = []
+        if len(nanind) == 0:
+            return gaps
         gapStartInd = nanind[0]
         gapStartWvl = nanwvl[0]
         for i in range(0,len(nanind) - 0):
@@ -275,8 +359,22 @@ whether large gaps exist using the module function SNIDsn.largeGapsInRange().
         return
 
 
+    def smoothSpectrum(self, phase, velcut, plot=False):
+        """
+Uses the Modjaz et al method to smooth a spectrum.
+        """
+        spec = np.copy(self.data[phase])
+        wsmooth, fsmooth, sepvel = smooth(self.wavelengths, spec, velcut)
+        self.smoothinfo[phase] = sepvel
+        self.data[phase] = fsmooth
 
-
+        if plot:
+            fig = plt.figure(figsize=(15,5))
+            plt.plot(self.wavelengths, spec, 'r', label='pre-smooth')
+            plt.plot(self.wavelengths, self.data[phase], 'k', label='smooth')
+            plt.legend(title=self.header['SN'])
+            return fig
+        return
 
 
 
